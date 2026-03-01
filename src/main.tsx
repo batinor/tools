a71c7b24581b23886c4eb7ff006cb065e7c0a8e5
Alban Caré <alban.care@gmail.com>
2026-03-01 17:07:01 +0100

    feat: Implement routing with React Router and add Home and Contact pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { Home, Contact } from './pages';
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: '/tools/',
+    element: <App />,
+    children: [
+      {
+        path: '/tools/',
+        element: <Home />,
+      },
+      {
+        path: '/tools/contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')! as HTMLElement).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
